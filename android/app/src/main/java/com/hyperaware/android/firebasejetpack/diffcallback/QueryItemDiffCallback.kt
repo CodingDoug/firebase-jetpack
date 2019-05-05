274674905cfe142b8593152b191721e39ffdf6c3
Doug Stevenson <doug@hyper-aware.com>
2019-05-05 15:48:47 -0700

    Suppress lint warning for diffutil callbacks using equals(). It's OK for data classes.

--- android/app/src/main/java/com/hyperaware/android/firebasejetpack/diffcallback/QueryItemDiffCallback.kt
+++ android/app/src/main/java/com/hyperaware/android/firebasejetpack/diffcallback/QueryItemDiffCallback.kt
@@ -1,5 +1,22 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hyperaware.android.firebasejetpack.diffcallback
 
+import android.annotation.SuppressLint
 import android.support.v7.util.DiffUtil
 import com.hyperaware.android.firebasejetpack.repo.QueryItem
 
@@ -14,6 +31,7 @@ open class QueryItemDiffCallback<T> : DiffUtil.ItemCallback<QueryItem<T>>() {
         return oldItem.id == newItem.id
     }
 
+    @SuppressLint("DiffUtilEquals")  // equals() is OK for data classes
     override fun areContentsTheSame(oldItem: QueryItem<T>, newItem: QueryItem<T>): Boolean {
         return oldItem.item == newItem.item
     }
--- android/app/src/main/java/com/hyperaware/android/firebasejetpack/diffcallback/QueryItemOrExceptionDiffUtilItemCallback.kt
+++ android/app/src/main/java/com/hyperaware/android/firebasejetpack/diffcallback/QueryItemOrExceptionDiffUtilItemCallback.kt
@@ -1,5 +1,22 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hyperaware.android.firebasejetpack.diffcallback
 
+import android.annotation.SuppressLint
 import android.support.v7.util.DiffUtil
 import com.hyperaware.android.firebasejetpack.repo.QueryItemOrException
 
@@ -19,6 +36,7 @@ class QueryItemOrExceptionDiffUtilItemCallback<T> : DiffUtil.ItemCallback<QueryI
         }
     }
 
+    @SuppressLint("DiffUtilEquals")  // equals() is OK for data classes
     override fun areContentsTheSame(oldItem: QueryItemOrException<T>, newItem: QueryItemOrException<T>): Boolean {
         return oldItem.data == newItem.data
     }
